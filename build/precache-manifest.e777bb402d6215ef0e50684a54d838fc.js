203d0bddcafae2a3dcf642ccfd43d7c29143c85f
vcccaat <yzhuba@connect.ust.hk>
2020-06-05 17:11:21 +0800

    publish gh-page

---
+++ build/precache-manifest.e777bb402d6215ef0e50684a54d838fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7f1e2c0fe416e082a6b973e8d909d1",
+    "url": "/vcccaat/AnimalCrossingDB/index.html"
+  },
+  {
+    "revision": "878b901b9c0e44b51e3c",
+    "url": "/vcccaat/AnimalCrossingDB/static/css/2.abd97b8c.chunk.css"
+  },
+  {
+    "revision": "17c3c8f7e5f8e202bb91",
+    "url": "/vcccaat/AnimalCrossingDB/static/css/main.31994dab.chunk.css"
+  },
+  {
+    "revision": "878b901b9c0e44b51e3c",
+    "url": "/vcccaat/AnimalCrossingDB/static/js/2.17125014.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vcccaat/AnimalCrossingDB/static/js/2.17125014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c3c8f7e5f8e202bb91",
+    "url": "/vcccaat/AnimalCrossingDB/static/js/main.f11548b4.chunk.js"
+  },
+  {
+    "revision": "89b7ede10729e9826e36",
+    "url": "/vcccaat/AnimalCrossingDB/static/js/runtime-main.85f2b331.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/vcccaat/AnimalCrossingDB/static/media/SEGA.12a3db8b.woff"
+  }
+]);
